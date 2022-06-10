f68ab75455a3af452701275170aeb5a5390cc6c7
Alison Lu <alibob12401@hotmail.com>
2022-06-10 16:29:56 -0400

    modified nlp.py

--- cloudmesh/nlp/nlp.py
+++ cloudmesh/nlp/nlp.py
@@ -12,15 +12,20 @@ class Nlp:
 
     def start(self):
         print("start")
+        # TODO: figure out how to start service
+
 
     def stop(self):
         print("stop")
+        # TODO: figure out how to stop service
 
     def info(self):
         print("info")
+        # TODO: figure out how to acquire info on service
 
     def status(self):
         print("status")
+        # TODO: figure out how to acquire status
 
     def run(self, source=None, output=None, parameter=None, text=None):
         print(f"Text: {text}")
